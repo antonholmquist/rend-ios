bf8391225c4513a22024cecaf3f4eddde81f4a6c
Anton Holmquist <anton.holmquist@monterosa.se>
2012-06-26 00:04:43 +0200

    Added Example Collection

---
+++ Examples/Rend Example Collection/Rend Example Collection/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rend Example Collection
+//
+//  Created by Anton Holmquist on 6/26/12.
+//  Copyright (c) 2012 Monterosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Examples/Rend Example Collection/Rend Example Collection/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rend Example Collection
+//
+//  Created by Anton Holmquist on 6/26/12.
+//  Copyright (c) 2012 Monterosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
